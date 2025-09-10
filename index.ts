f77bbde6795f111f2c62ff80540b22079ac0abd6
202319060410 <202319060410@lab01b-l>
2025-09-09 21:47:50 -0400

    chore: initial installations

---
+++ index.ts
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = express();
+
+// const url = "https://score.hsborges.dev/api"
---
+++ proxy.ts
@@ -0,0 +1,15 @@
+/**
+ * Nesta atividade você deverá submeter a base do serviço proxy do desafio.
+ * Você deverá já propor uma base da aplicação com a aplicação de padrões de projeto.
+ * Alguns padrões de destaque neste início (sugestões, que podem ou não ser empregados):
+ * - Command: encapsulamento das requisições
+ * - Singleton: garantir a existência de um único cliente do serviço ou fila
+ * - Decorator: adição de capacidades dinâmicas ao serviço, como organização das chamadas, cache, etc. 
+ * - Iterator: iteração sobre a fila de requisições, inclusive, podendo ser usado para priorizações
+ * - Observer: para notificação de atividades.
+ * 
+ * DICAS:
+- 
+ */
+
+const url = "https://score.hsborges.dev/api"
